62f1fd0a8fc4209e2bf5775826c46412ed8934e0
Nathalia Nascimento <nathalianascimento78@hotmail.com>
2019-11-28 08:43:36 -0300

    SSD1306_SPI lib

--- src/Sensor_temperatura_PT100/Sensor_temperatura_PT100.ino
+++ src/Sensor_temperatura_PT100/Sensor_temperatura_PT100.ino
@@ -1,4 +1,4 @@
- * /************************************************************* 
+ /************************************************************* 
  *  AQUISIÇÃO DOS DADOS DE TEMPERATURA DO SENSOR PT100       *
  *  AUTOR: NATHALIA NASCIMENTO                               *
  *  DATA : 13/10/2019                                        *
@@ -16,10 +16,9 @@ CS: Chip Select
 */
 #include <SPI.h>
 #include "SSD1306_SPI.h"
+#include<avr/io.h>
 
-//#include<avr/io.h>
-
-#define USE_FRAME_BUFFER
+//#define USE_FRAME_BUFFER
 
 #ifdef USE_FRAME_BUFFER
 SSD1306_SPI_FB oled;
@@ -27,42 +26,52 @@ SSD1306_SPI_FB oled;
 SSD1306_SPI oled;
 #endif
 
+void setup(void)
+{
+  Serial.begin(9600);
 
-void print_ad(void) {
+  //AD CONFIG
+  ADMUX = 0b000100001;    
+  ADCSRA |= (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN);     //PRESCALER FREQ E DESABILITA ADC
+  ADCH = 0;
   
-}
-//  
-//void adc1_setup(void){
-//
-//ADMUX = 0b000100001;    
-//
-//ADCSRA |= (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN);     //PRESCALER FREQ E DESABILITA ADC
-//
-//}
-//
-//int ler_ad(void){
-//  
-//  ADCSRA |= (1 << ADSC);
-//  
-//  while (ADCSRA & (1 << ADSC));
-// 
-//  return ADCH;
-//}
-
-void setup(void){
   oled.begin();
-  oled.print("Sensor temperatura PT100");
-
-  #ifdef USE_FRAME_BUFFER
-  oled.renderAll();
-  #endif
-
-  delay(1000);
-  oled.clear();
-}
   
-void loop(void){
-  size_t len;
   oled.gotoXY(5,3);
-  oled.print("PT100: ");
- }
+  oled.print(F("SENSOR TEMPERATURA"));
+
+  oled.gotoXY(40,5);
+  oled.print(F("PT100"));
+  
+#ifdef USE_FRAME_BUFFER
+  oled.renderAll();
+#endif
+  delay(3000);
+}
+
+int ler_ad(void){
+  
+  ADCSRA |= (1 << ADSC);
+  
+  while (ADCSRA & (1 << ADSC));
+ 
+  return ADCH;
+}
+
+void loop(void) 
+{
+ 
+  oled.clear();
+  oled.gotoXY(5,3);
+  oled.print(F("PT100 : "));
+  int ad = ler_ad();
+
+  oled.gotoXY(70,3);
+  oled.print(ad);
+  
+#ifdef USE_FRAME_BUFFER
+  oled.renderAll();
+#endif
+  delay(3000);
+  
+}
